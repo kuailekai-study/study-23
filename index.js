efcb7b1020632ef2e200c6cef8450ac2ee1eeaf5
kuailekai <61490879+kuailekai@users.noreply.github.com>
2020-10-24 08:26:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const select = document.querySelector('select');
+const para = document.querySelector('p');
 
+select.addEventListener('change', LAU);
+
+function LAU() {
+  let choice = select.value;
+  
+  if (choice === "zh-cn") {
+    para.textContent = "你好";
+  } else if (choice === "zh-tw") {
+    para.textContent = "你好";
+  } else if (choice === "en-us") {
+    para.textContent = "Hello";
+  } else if (choice === "en-uk") {
+    para.textContent = "Hello";
+  } else {
+    para.textContent = "";
+  }
+}
