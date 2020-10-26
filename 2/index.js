a182479a00cfc7ed36eb6cf680c2827c514f9c86
kuailekai <61490879+kuailekai@users.noreply.github.com>
2020-10-26 15:38:48 +0800

    Update index.js

--- 2/index.js
+++ 2/index.js
@@ -1,6 +1,6 @@
 const para = document.querySelector('p');
 
-para.addEventListener('click', updateName);
+para.addEventListener("click", updateName);
 
 function updateName() {
   let name = prompt("输入你的名字");
